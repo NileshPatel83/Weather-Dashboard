7975df6339915512fbb7d1e64608e0165b7951df
NileshPatel83 <hselin_83@yahoo.com>
2023-02-06 16:49:14 +1000

    addCurrentTemperatureInformation method added to display current temp and feels like temp.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,33 @@ const reqCityNameURL = 'https://api.openweathermap.org/geo/1.0/reverse?';
 const apiKey = '54235d685be1b7eea306dd40934a9322';
 
 //ID Names.
-const cityNameID = 'city-name';
-const presentDayID = 'present-day-name';
-const currentTempID = 'current-temperature';
-const feelsLikeID = 'feels-like';
-const currentWeatherIconID = 'current-weather-icon';
-const currentWeatherDescID = 'current-weather-description';
-const currentHighTempID = 'current-high-temperature';
-const currentLowTempID = 'current-low-temperature';
-const currentWindID = 'current-wind';
-const currentHumidityID = 'current-humidity';
-const currentSunriseID = 'current-sunrise';
-const currentSunsetID = 'current-sunset';
+//const cityNameID = 'city-name';
+//const presentDayID = 'present-day-name';
+// const currentTempID = 'current-temperature';
+// const feelsLikeID = 'feels-like';
+// const currentWeatherIconID = 'current-weather-icon';
+// const currentWeatherDescID = 'current-weather-description';
+// const currentHighTempID = 'current-high-temperature';
+// const currentLowTempID = 'current-low-temperature';
+// const currentWindID = 'current-wind';
+// const currentHumidityID = 'current-humidity';
+// const currentSunriseID = 'current-sunrise';
+// const currentSunsetID = 'current-sunset';
 
 //Class Names
-const flexRowJustMiddle = 'flex-row-justify-middle';
+const flexRowJustMiddleCl = 'flex-row-justify-middle';
+const flexColJustMiddleCl = 'flex-column-justify-middle';
+const todayCl = 'today';
+const currCondCl = 'current-condition';
+const flexJustMiddleCl = 'flex-justify-middle';
+const currDataCl = 'current-data';
+const currTempCl = 'current-temp';
+const feelLikeCl = 'feels-like';
+const currWeatherIconCl = 'current-weather-icon';
+const currWeatherDescCl = 'current-weather-description';
+const currOthInfoCl = 'current-other-info';
+const lineHgt2remCl = 'line-height-2rem';
+
 
 //DOM Elements
 const presentDayContainer = document.getElementById('present-day');
@@ -50,78 +62,120 @@ async function init(){
     let cityName = await getCityName(cityLocation);
     if(cityName === '') return;
 
+    //Gets current weather information.
+    let currentWeatherData = await getCurrentWeatherData(cityLocation);
+    console.log(currentWeatherData);
+
     //Removes temporary loading text once weather data is obtained.
     h2El.remove();
+
+    //Adds city name, current day and date information.
+    addLocationInformation(cityName);
+    
+    //Adds current weather information.
+    addsCurrentWeatherInformation(currentWeatherData);
+
+    //Adds 5 day forecast information.
+}
+
+//Updates current weather information in browser.
+function addsCurrentWeatherInformation(currentWeatherData){
+
+    //Creates div container that holds all weather data.
+    let currCondEl = document.createElement('div');
+    currCondEl.className = `${currCondCl} ${flexJustMiddleCl}`;
+
+    //Adds current temperature information.
+    addCurrentTemperatureInformation(currentWeatherData, currCondEl);
+
+    // //Updates current temperature value.
+    // let currentTempEL = document.getElementById(currentTempID);
+    // let currentTemp = Math.round(currentWeatherData.main.temp * 10)/ 10;
+    // currentTempEL.innerHTML = `<img src="/assets/images/Temperature.png" alt="Temperature icon">${currentTemp}&#176C`;
+
+    // //Updates feels like value.
+    // let feelsLikeEL = document.getElementById(feelsLikeID);
+    // let feelsLike = Math.round(currentWeatherData.main.feels_like * 10)/ 10;
+    // feelsLikeEL.innerHTML = `<img src="/assets/images/FeelsLike.png" alt="Feels like icon">${feelsLike}&#176C`;
+
+    // //Gest URL for weather icon.
+    // let iconURL = getIconURL(currentWeatherData.weather[0].icon);
+
+    // //Updates current weather icon.
+    // let currWeatherIconEL = document.getElementById(currentWeatherIconID);
+    // currWeatherIconEL.src = iconURL;
+
+    // //Gets current weather description with first letter capital in each word.
+    // let currWeatherDesc = getWeatherDescription(currentWeatherData.weather[0].description);
+
+    // //Updates current weather description.
+    // let currWeatherDescEL = document.getElementById(currentWeatherDescID);
+    // currWeatherDescEL.innerHTML = currWeatherDesc;
+
+    // let currHighTempEL = document.getElementById(currentHighTempID);
+
+    presentDayContainer.append(currCondEl);
+}
+
+//Adds current temperature information.
+function addCurrentTemperatureInformation(currentWeatherData, currCondEl){
+
+    //Creates div element for current temperature information.
+    let currDataEl = document.createElement('div');
+    currDataEl.className = currDataCl;
+
+    //Gets current temperature with one decimal place.
+    let currentTemp = Math.round(currentWeatherData.main.temp * 10)/ 10;
+
+    //Create div element to display current temperature.
+    let currTempEl = document.createElement('div');
+    currTempEl.className = currTempCl;
+    currTempEl.innerHTML = `<img src="/assets/images/Temperature.png" alt="Temperature icon">${currentTemp}&#176C`;
+
+    //Gets feel like temperature with one decimal place.
+    let feelsLike = Math.round(currentWeatherData.main.feels_like * 10)/ 10;
+
+     //Create div element to display feels like temperature.
+    let feelsLikeEL = document.createElement('div');
+    feelsLikeEL.className = `${feelLikeCl} ${flexRowJustMiddleCl}`
+    feelsLikeEL.innerHTML = `<img src="/assets/images/FeelsLike.png" alt="Feels like icon">${feelsLike}&#176C`;
+
+    currDataEl.append(currTempEl, feelsLikeEL);
+
+    currCondEl.append(currDataEl);
+}
+
+//Adds city name, current day and date information.
+function addLocationInformation(cityName){
 
     //Adds the city name element in browser.
     addCityName(cityName);
 
-    //Updates value of present day and date.
-    updatePresentDay();
-
-    //Gets current weather information.
-    let currentWeatherData = await getCurrentWeatherData(cityLocation);
-    console.log(currentWeatherData);
-    
-    //Updates current weather information in browser.
-    updateCurrentWeatherInformation(currentWeatherData);
+    //Adds value of present day and date.
+    addPresentDay();
 }
 
-//Adds temporary loading text while weather data is obtained.
-function addTemporaryLoadingText(){
-    let h2El = document.createElement('h2');
-    h2El.textContent = "Loading..."
-    presentDayContainer.append(h2El);
+//Updates value of present day and date.
+function addPresentDay(){
 
-    return h2El;
+    //Adds div element and updates its inner HTML to present day and date.
+    let dayNameEl = document.createElement('div');
+    //dayNameEl.id = presentDayID;
+    dayNameEl.className = todayCl;
+    dayNameEl.innerHTML = dayjs().format('dddd- DD/MM/YYYY');
+    presentDayContainer.append(dayNameEl);
 }
 
-//Updates current weather information in browser.
-function updateCurrentWeatherInformation(currentWeatherData){
+//Adds the city name element in browser.
+function addCityName(cityName){
 
-    //Updates current temperature value.
-    let currentTempEL = document.getElementById(currentTempID);
-    let currentTemp = Math.round(currentWeatherData.main.temp * 10)/ 10;
-    currentTempEL.innerHTML = `<img src="/assets/images/Temperature.png" alt="Temperature icon">${currentTemp}&#176C`;
-
-    //Updates feels like value.
-    let feelsLikeEL = document.getElementById(feelsLikeID);
-    let feelsLike = Math.round(currentWeatherData.main.feels_like * 10)/ 10;
-    feelsLikeEL.innerHTML = `<img src="/assets/images/FeelsLike.png" alt="Feels like icon">${feelsLike}&#176C`;
-
-    //Gest URL for weather icon.
-    let iconURL = getIconURL(currentWeatherData.weather[0].icon);
-
-    //Updates current weather icon.
-    let currWeatherIconEL = document.getElementById(currentWeatherIconID);
-    currWeatherIconEL.src = iconURL;
-
-    //Gets current weather description with first letter capital in each word.
-    let currWeatherDesc = getWeatherDescription(currentWeatherData.weather[0].description);
-
-    //Updates current weather description.
-    let currWeatherDescEL = document.getElementById(currentWeatherDescID);
-    currWeatherDescEL.innerHTML = currWeatherDesc;
-
-    let currHighTempEL = document.getElementById(currentHighTempID);
-    //currHighTempEL.textContent = 
-}
-
-//Gest URL for weather icon.
-function getIconURL(icon){
-    return `https://openweathermap.org/img/wn/${icon}@2x.png`;
-}
-
-//Gets current weather description with first letter capital in each word.
-function getWeatherDescription(description){
-
-    let descArray = description.split(' ');
-
-    for (let i = 0; i < descArray.length; i++) {
-        descArray[i] = descArray[i].charAt(0).toUpperCase() + descArray[i].substring(1);
-    }
-
-    return descArray.join(' ');
+    //Creates h2 element, adds id and class name.
+    //Sets inner html text to show city name.
+    let cityNameEl = document.createElement('h2');
+    //cityNameEl.id = cityNameID;
+    cityNameEl.className = flexRowJustMiddleCl;
+    cityNameEl.innerHTML = `<img class="location-tag" src="/assets/images/Location.png" alt="Location pin icon">${cityName}`;
+    presentDayContainer.append(cityNameEl);
 }
 
 //Gets current weather information.
@@ -132,26 +186,6 @@ async function getCurrentWeatherData(cityLocation){
     const response = await fetch(url);
 
     return response.json();
-}
-
-//Updates value of present day and date.
-function updatePresentDay(){
-
-    //Gets the day name div element and updates its inner HTML.
-    let dayNameEl = document.getElementById(presentDayID);
-    dayNameEl.innerHTML = dayjs().format('dddd- DD/MM/YYYY');
-}
-
-//Adds the city name element in browser.
-function addCityName(cityName){
-
-    //Creates h2 element, adds id and class name.
-    //Sets inner html text to show city name.
-    let cityNameEl = document.createElement('h2');
-    cityNameEl.id = cityNameID;
-    cityNameEl.className = flexRowJustMiddle;
-    cityNameEl.innerHTML = `<img class="location-tag" src="/assets/images/Location.png" alt="Location pin icon">${cityName}`;
-    presentDayContainer.append(cityNameEl);
 }
 
 //Gets the city name from lat and lon.
@@ -287,4 +321,13 @@ function getLocalStorage(){
 //Logs the error in consol log.
 function error(err) {
     //console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+//Adds temporary loading text while weather data is obtained.
+function addTemporaryLoadingText(){
+    let h2El = document.createElement('h2');
+    h2El.textContent = "Loading..."
+    presentDayContainer.append(h2El);
+
+    return h2El;
 }
